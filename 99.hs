88bd1194c87e2694a1780f79eab5f827c13b2dbd
zhangpan07 <zhangpan07@meituan.com>
2016-11-17 22:10:41 +0800

    get nth item of list

--- 99.hs
+++ 99.hs
@@ -4,8 +4,9 @@ let arr = [11, 2, 9, 6, 40]
 last arr
 -- 2
 -- last but one element of a list.
-last $ take (length arr - 1) arr
+last $ init arr
 -- 3
 -- ind the K'th element of a list. The first element in the list is number 1.
 elementAt :: (Num a) => a -> [b] -> b
-elementAt
+elementAt k > length = arr -> 0
+elementAt k arr = arr!!k
